5d61dc5e6910cc01edefca41b314348e3b997ab9
hg_yellow <jang010505@gmail.com>
2024-06-26 01:05:21 +0900

    Init project

---
+++ src/main/java/com/hgyellow/querydsl/common/config/QueryDSLConfiguration.java
@@ -0,0 +1,20 @@
+package com.hgyellow.querydsl.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.persistence.EntityManager;
+
+@Configuration
+public class QueryDSLConfiguration {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
